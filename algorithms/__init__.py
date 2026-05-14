8c1ab4f6ad225f469a3a71a48eb2e9c3487c3334
aqiah <haiqausman98@gmail.com>
2026-05-14 20:23:17 +0500

    police init

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -3,5 +3,8 @@ from .road_network import RoadNetworkBuilder, UnionFind
 from .genetic_algorithm import GeneticAlgorithmSolver
 from .astar_router import AStarRouter
 
+from .police_deployment import allocate_police_positions, POLICE_COUNT, NEIGHBOR_PENALTY
+
 __all__ = ["CSPSolver", "RoadNetworkBuilder", "UnionFind",
-           "GeneticAlgorithmSolver", "AStarRouter"]
+           "GeneticAlgorithmSolver", "AStarRouter",
+           "allocate_police_positions", "POLICE_COUNT", "NEIGHBOR_PENALTY"]
